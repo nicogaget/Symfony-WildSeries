bc87c11e307f64c2f0f2c29db91470e2bcda29aa
Nico <n.gaget@gmail.com>
2020-07-06 23:32:10 +0200

    entities

---
+++ src/Entity/Season.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SeasonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SeasonRepository::class)
+ */
+class Season
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $number;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $year;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $descritpion;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumber(): ?int
+    {
+        return $this->number;
+    }
+
+    public function setNumber(int $number): self
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(?int $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getDescritpion(): ?string
+    {
+        return $this->descritpion;
+    }
+
+    public function setDescritpion(string $descritpion): self
+    {
+        $this->descritpion = $descritpion;
+
+        return $this;
+    }
+}
